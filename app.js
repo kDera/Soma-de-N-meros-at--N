d93f082b238e30b442cdf343700bca5a29e562c6
kDera <caduvazquezabj@gmail.com>
2024-02-06 17:59:15 -0300

    v1

---
+++ app.js
@@ -0,0 +1,12 @@
+
+function somador(){
+    var resultAtual = parseInt(document.getElementById("resultAtual").value);
+
+    if(resultAtual % 2 === 0){
+        document.getElementById("resultado").innerText = "o numero "+ resultAtual+"é PAR";
+    }
+    else{
+        document.getElementById("resultado").innerText = "o numero é "+ resultAtual+" IMPAR";
+    }
+
+}
